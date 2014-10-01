bb0bb455c820e7775c37f54f3b5e9dd467c7580e
unknown <zesssalamander27@hotmail.com>
2014-09-30 19:32:37 -0500

    Base del pproyecto

---
+++ IVAProg/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 30/09/2014
+ * Time: 07:26 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace IVAProg
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
